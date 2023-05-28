fb176f2f9881b505d6a92989d856baaa922ca84c
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-05-28 12:41:32 +0200

    add an other logic condition: v-else-if

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const app = Vue.createApp({ //qua inseriamo il nostro option object
             description: 'Perfect for the winter',
             image :'./assets/images/socks_green.jpg',
             // url: 'http://www.google.it'
-            inStock: true, // CONDIZIONE V-IF: assegno un valore booleano 'true'. Se lo imposto su false, nel DOM verrà mostrata l'opzione per cui verrà visualizzato il tag p con condizione v-else.
+            //inStock: false, // CONDIZIONE V-IF: assegno un valore booleano 'true'. Se lo imposto su false, nel DOM verrà mostrata l'opzione per cui verrà visualizzato il tag p con condizione v-else.
+            //CONDIZIONE V-SHOW: Se imposto il valore booleano su 'false', nel DOM verrà visualizzato un 'display: none' relativo al valore, quindi verrà solo reso invisibile.
+            inventory: 0 //8 //100 //CONDIZIONE V-ELSE-IF: Aggiungo questo dato per aggiungere un altro livello di condizoone logica. Se setto il valore a 100 a condizione sarà vera e quindi visualizzarò 'In Stock'. Se modifico da 100 a 8, stando alla condizoone logica che abbiamo riportato nel DOM, dovremmo visualizzare 'Almost sold out' oppure 'Out of Stock' se settiamo il valore a 0
             
         }
     }
