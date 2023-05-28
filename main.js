cca4c6013176213a01be45645a28998fd3a42713
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-05-28 12:22:01 +0200

    v-condition

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const product = 'Socks'
+const app = Vue.createApp({ //qua inseriamo il nostro option object
+    data(){ //data option che ritornano il valore product
+        return{
+            product: 'Socks', //Posso anche modificare il valore attuale con un altro e il mio DOM riporta la modifica istantaneamente. Tutto questo è possibile perche Vue ha un sistema reattivo ('Reactivity System')
+            description: 'Perfect for the winter',
+            image :'./assets/images/socks_green.jpg',
+            // url: 'http://www.google.it'
+            inStock: true, // CONDIZIONE V-IF: assegno un valore booleano 'true'. Se lo imposto su false, nel DOM verrà mostrata l'opzione per cui verrà visualizzato il tag p con condizione v-else.
+            
+        }
+    }
+})
